486bf7b796caddbb9b41c519934ed760b79e2ce6
Elliot Goodrich <elliotgoodrich@gmail.com>
2014-11-24 00:46:51 +0000

    Fix sieve_of_eratosthenes example

--- examples/sieve_of_eratosthenes.cpp
+++ examples/sieve_of_eratosthenes.cpp
@@ -5,17 +5,17 @@
 #include <iostream>
 
 int main() {
-	auto const limit = 3;
+	auto const limit = 100;
 	std::array<bool, limit> is_prime;
 	is_prime.fill(true);
 
 	is_prime[0] = false;
 	is_prime[1] = false;
 
-	std::cout << "Primes under " << limit << "\n";
-	for(auto i : ez::make_interval[0](limit)) {
+	std::cout << "Primes under " << limit << ":\n";
+	for(auto&& i : ez::make_interval[2](limit)) {
 		if(is_prime[i]) {
-			for(auto n : ez::make_interval[2](limit / i)) {
+			for(int n = 2; n < limit / i; ++n) {
 				is_prime[i * n] = false;
 			}
 			std::cout << i << ", ";
